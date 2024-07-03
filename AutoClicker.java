9d5ee0589890995405ef621d767d78eff550f0c0
alfardil <fardilfayed@gmail.com>
2024-07-03 14:49:19 -0400

    Update AutoClicker.java

--- AutoClicker.java
+++ AutoClicker.java
@@ -7,13 +7,15 @@ public class AutoClicker {
             while (true) {
                 Robot r = new Robot();
                 int button = InputEvent.BUTTON1_DOWN_MASK;
-                System.out.print("Click");
-                r.mousePress(button);
-                Thread.sleep(400);
-                r.mouseRelease(button);
+                System.out.println("Click");
 
-                // each second is 1000 milliseconds
-                Thread.sleep(2 * 1000);
+                // presses down for some duration
+                r.mousePress(button);
+                Thread.sleep(100);
+                // then lets go
+                r.mouseRelease(button);
+                // how long between each click. 1 second is 1000 milliseconds
+                Thread.sleep(500);
             }
         } catch (Exception e) {
             e.printStackTrace();
